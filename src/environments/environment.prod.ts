4ee848e1e7bb2cddf050de39cb32ff4d54e1ca54
TatsuyaSSK <ssktty514@gmail.com>
2021-02-26 11:07:55 +0900

    feat: github actionsの設定

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAzzNXxfPcfIEbD38lAMqsh4Wf-JCV3F3U',
+    authDomain: 'pridetimer.firebaseapp.com',
+    projectId: 'pridetimer',
+    storageBucket: 'pridetimer.appspot.com',
+    messagingSenderId: '583175667379',
+    appId: '1:583175667379:web:24eb7bf17f468dc4897ac6',
+    measurementId: 'G-G99G4R7EJX',
+  },
 };
