d77313cd14dddc22ef7007860d71f0fdfc50524e
TatsuyaSSK <ssktty514@gmail.com>
2021-02-26 10:31:28 +0900

    feat: 環境構築

---
+++ prideTimer/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireFunctionsModule, REGION } from '@angular/fire/functions';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAnalyticsModule,
+    AngularFirestoreModule,
+    AngularFireFunctionsModule,
+    AngularFireStorageModule,
+    AngularFireAuthModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [
+    {
+      provide: REGION,
+      useValue: 'asia-northeast1',
+    },
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ prideTimer/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAzzNXxfPcfIEbD38lAMqsh4Wf-JCV3F3U',
+    authDomain: 'pridetimer.firebaseapp.com',
+    projectId: 'pridetimer',
+    storageBucket: 'pridetimer.appspot.com',
+    messagingSenderId: '583175667379',
+    appId: '1:583175667379:web:24eb7bf17f468dc4897ac6',
+    measurementId: 'G-G99G4R7EJX',
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
