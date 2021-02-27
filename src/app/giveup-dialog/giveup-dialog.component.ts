c02cafc922de6b113f7bec9b3ff548e30006ac10
TatsuyaSSK <ssktty514@gmail.com>
2021-02-28 00:48:43 +0900

    タスク取得、タスクステータス更新

--- src/app/giveup-dialog/giveup-dialog.component.ts
+++ src/app/giveup-dialog/giveup-dialog.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { AngularFireFunctions } from '@angular/fire/functions';
+import { TaskService } from '../services/task.service';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-giveup-dialog',
   templateUrl: './giveup-dialog.component.html',
-  styleUrls: ['./giveup-dialog.component.scss']
+  styleUrls: ['./giveup-dialog.component.scss'],
 })
 export class GiveupDialogComponent implements OnInit {
+  data$;
+  taskId: string;
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: { taskId: string },
+    private taskService: TaskService,
+    private afn: AngularFireFunctions,
+    private authService: AuthService
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  fail() {
+    this.taskService.updateTaskStatusFailure(this.data.taskId);
+    const callable = this.afn.httpsCallable('subtractPoint');
+    return callable({}).toPromise();
   }
-
 }
