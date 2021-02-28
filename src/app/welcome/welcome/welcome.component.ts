51f629d3c1f9c87fe8bfdcda03e326c2b2890da7
komura <affirman03139@outlook.jp>
2021-02-28 11:31:33 +0900

    feat(welcome): welcome途中

--- src/app/welcome/welcome/welcome.component.ts
+++ src/app/welcome/welcome/welcome.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
-import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-welcome',
@@ -8,6 +7,14 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./welcome.component.scss']
 })
 export class WelcomeComponent implements OnInit {
+  isLoadingImg = true;
+  usageSteps = [
+    `discordチャンネルでルームを作成\nチャンネルの編集、連携サービスから\nウェブフックを作成しよう！\n(権限がない時はできないため、\nチャンネル管理者に相談しましょう。)`,
+    `ログイン後ウェブフックを入力すれば準備完了！`,
+    `＋ボタンでタイマーをセットして\nやるべきことに取り組もう💪`,
+    `時間内に達成しないと、\ndiscordチャンネルに罰ゲームと共に\n失敗が晒されてしまう...😭\n逆に達成すると、レベルアップ！\nみんなで競い合おう！`,
+  ];
+
   constructor(
     public authService: AuthService,
   ) { }
